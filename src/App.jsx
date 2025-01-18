9df9770b0313680f66db259b8821d61f69c128d9
Nicholas Lam <nicholas.lfh91@gmail.com>
2025-01-18 14:29:32 +0800

    feat:home layout

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// src/App.jsx
+
+const App = () => {
+  return (
+
+<>
+<div className="NavBar"/>
+
+<ul>
+  <li>Home</li>
+  <li>MailBoxes</li>
+  <li>New Mailbox</li>
+</ul>
+
+<h1>Mailbox List</h1>
+
+<ul>
+  <li>Mailbox 1</li>
+  <li>Mailbox 2</li>
+  <li>Mailbox 3</li>
+</ul>
+
+
+</>
+  )
+};
+
+export default App;
